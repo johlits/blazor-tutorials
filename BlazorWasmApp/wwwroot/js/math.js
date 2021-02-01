9bafa5b5f344f9671e4ee1a5b4371e76d7e7f392
Johan Litsfeldt <johlits@gmail.com>
2021-02-01 15:14:37 +0100

    lesson 8

---
+++ BlazorWasmApp/wwwroot/js/math.js
@@ -0,0 +1,12 @@
+﻿window.math = {
+    square: function (a) {
+        return a * a;
+    },
+    logBinaryAsync: function (dec) {
+        DotNet.invokeMethodAsync('BlazorWasmApp',
+            'ReturnBinaryAsync', dec)
+            .then(data => {
+                console.log(data);
+            });
+    }
+}
