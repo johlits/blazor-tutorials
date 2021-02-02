753fd24cacdbac0a672c62d069d123738cc90072
Johan Litsfeldt <johlits@gmail.com>
2021-02-02 14:16:37 +0100

    lesson 9

--- BlazorWasmApp/Program.cs
+++ BlazorWasmApp/Program.cs
@@ -19,6 +19,8 @@ namespace BlazorWasmApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+
             await builder.Build().RunAsync();
         }
     }
